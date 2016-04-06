4c024e66efee16f12ec418d430c2e9e0d3bf40f5
wanghao1891 <wanghao1891@126.com>
2016-04-06 12:15:40 +0800

    Add ddp client.

--- index.ios.js
+++ index.ios.js
@@ -11,7 +11,72 @@ import React, {
   View
 } from 'react-native';
 
+if (typeof process === 'undefined') process = {};
+process.nextTick = setImmediate;
+global.process = process;
+
+var DDPClient = require('ddp-client');
+
 class familymobile extends Component {
+  componentDidMount() {
+    //var ddpClient = new DDPClient({url: 'ws://localhost:3000/websocket'});
+
+    var ddpclient = new DDPClient({
+      // All properties optional, defaults shown
+      host : "localhost",
+      port : 3000,
+      ssl  : false,
+      autoReconnect : true,
+      autoReconnectTimer : 500,
+      maintainCollections : true,
+      ddpVersion : '1',  // ['1', 'pre2', 'pre1'] available
+      // Use a full url instead of a set of `host`, `port` and `ssl`
+      url: 'ws://localhost:3000/websocket',
+      socketConstructor: WebSocket // Another constructor to create new WebSockets
+    });
+
+//    var ws = new WebSocket('ws://localhost:3000/websocket');
+//
+//    ws.onopen = () => {
+//      // connection opened
+//      ws.send(JSON.stringify({"msg":"connect","version":"1","support":["1","pre2","pre1"]}));
+//    };
+//
+//    ws.onmessage = (e) => {
+//      // a message was received
+//      console.log(e.data);
+//    };
+//
+//    ws.onerror = (e) => {
+//      // an error occurred
+//      console.log(e.message);
+//    };
+//
+//    ws.onclose = (e) => {
+//      // connection closed
+//      console.log(e.code, e.reason);
+//    };
+
+    ddpclient.connect(function() {
+      ddpclient.subscribe('tasks', [], function() {
+        console.log('tasks:', ddpclient.collections.tasks);
+      });
+//      ddpclient.subscribe('tasks');
+    });
+
+//    // observe the lists collection
+//    var observer = ddpclient.observe('tasks');
+//    observer.added = function() {
+//      console.log(ddpclient.collections.tasks);
+//    };
+//    observer.changed = function() {
+//      console.log(ddpclient.collections.tasks);
+//    };
+//    observer.removed = function() {
+//      console.log(ddpclient.collections.tasks);
+//    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
